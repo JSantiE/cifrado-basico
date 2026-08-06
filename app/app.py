99df85b3e22fcc0eb3697704e0eb7c32420e82a3
JSantiE <josh1621@hotmail.com>
2026-08-05 19:57:34 -0500

    generando funciones

--- app/app.py
+++ app/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    #return "<h1>Hello, World!</h1>"
-    return render_template("index.html")
+    
+    abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    numeros = "0123456789"
+    
+    data = {
+        "title": "Index",
+        "abecedarioBase": abecedario,
+        "abecedarioGenerado": numeros,
+        "message": "Cifrado de datos - Trasposición de columnas",
+    }
+    
+    return render_template("index.html", data=data)
+
+@app.route("/generar", methods=["POST"])
+def generar():
+    print("Generando cifrado...")
+    return {"mensaje": "Cifrado generado"}
 
 if __name__ == "__main__":
     app.run(debug=True)
