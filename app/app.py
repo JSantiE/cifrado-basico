8c06d27b124b033aa21b5831d26ac26ac5c87c0b
JSantiE <josh1621@hotmail.com>
2026-08-07 19:56:32 -0500

    proceso trasposicion 90% finalizado

--- app/app.py
+++ app/app.py
@@ -2,15 +2,12 @@ from flask import Flask, render_template, request, url_for, redirect, jsonify, m
 
 app = Flask(__name__)
 
-abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numeros = "0123456789"
-
 @app.route("/")
 def index():
     
     data = {
         "title": "Index",
-        "abecedarioBase": abecedario,
+        "abecedarioBase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
         "message": "Cifrado de datos - Trasposición de columnas",
     }
     
@@ -19,46 +16,92 @@ def index():
 @app.route("/generar", methods=["POST"])
 def generar():
     req = request.get_json()    
-    abecedario_generado = abecedario
     
-    #Obtener los datos del formulario
     cantidad_letras = int(req.get("cantidad_letras"))    
     alfanumerico = req.get("alfanumerico")
     inverso = req.get("inverso")
     letraInicial = req.get("letraInicial")
     palabraClave = req.get("palabraClave")
     
-    #Generar abecedario con 27 letras si se selecciona la opción correspondiente
-    if cantidad_letras == 27:
-        abecedario_generado = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-    
-    #Agregar números al abecedario si se selecciona la opción correspondiente
-    if alfanumerico == True:
-        abecedario_generado += numeros
-    
-    #Invertir el abecedario si se selecciona la opción correspondiente
-    if inverso == True:
-        abecedario_generado = abecedario_generado[::-1]
-    
-    #Mover la letra inicial al inicio del abecedario si se proporciona una letra inicial
-    if letraInicial != "":
-        letraInicial = letraInicial.upper()
-        if letraInicial in abecedario_generado:
-            index = abecedario_generado.index(letraInicial)
-            abecedario_generado = abecedario_generado[index:] + abecedario_generado[:index]
-    
-    #Agregar la palabra clave al inicio del abecedario si se proporciona una palabra clave
-    if palabraClave != "":
-        palabraClave = palabraClave.upper()
-        palabraClave = "".join(sorted(set(palabraClave), key=palabraClave.index))
-        abecedario_generado = "".join([letra for letra in abecedario_generado if letra not in palabraClave])
-        abecedario_generado = palabraClave + abecedario_generado
-    
-    print("Datos recibidos:", abecedario_generado)
+    abecedario_generado = generar_abecedario(cantidad_letras, alfanumerico, inverso, letraInicial, palabraClave)
     
     res = make_response(jsonify({"abecedario_generado": list(abecedario_generado)}), 200)
     
     return res
+
+@app.route("/generarTrasposicion", methods=["POST"])
+def generarTrasposicion():
+    req = request.get_json()
+    
+    #abecedario = "POÑNMLKJZYDCBAQIHGFEXWVUTSR"
+    trasposicion = req.get("trasposicion")
+    abecedario = list(req.get("abecedario"))
+    
+    tblTrasposicion = []
+    
+    for i in range(0, len(abecedario), len(trasposicion)):
+        bloque = abecedario[i:i+len(trasposicion)]#separa el abecedario en bloques del tamaño de la trasposición
+        
+        fila = [
+            letra for _, letra in sorted(zip(trasposicion, bloque), key=lambda x: int(x[0]))#ordenar el bloque según la trasposición
+        ]
+        
+        tblTrasposicion.extend(fila)
+    
+    print("Datos recibidos:", tblTrasposicion)
+    
+    res = make_response(jsonify({"abecedario_generado": list(tblTrasposicion)}), 200)
+    
+    return res
+
+@app.route("/generarTrama", methods=["POST"])
+def generarTrama():
+    req = request.get_json()
+    
+    trama = req.get("trama")
+    
+    abecedario_generado = "3"
+    
+    res = make_response(jsonify({"abecedario_generado": list(abecedario_generado)}), 200)
+    
+    return res
+
+def generar_abecedario(cantidad_letras, alfanumerico, inverso, letraInicial, palabraClave):
+    abecedario_generado = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    if cantidad_letras == 27:
+        abecedario_generado = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    
+    if alfanumerico == True:
+        abecedario_generado += "0123456789"
+    
+    if inverso == True:
+        abecedario_generado = abecedario_generado[::-1]
+    
+    if letraInicial:
+        abecedario_generado = mover_letra_inicial(abecedario_generado, letraInicial)
+        
+    if palabraClave:
+        abecedario_generado = aplicar_palabra_clave(abecedario_generado, palabraClave)
+    
+    return abecedario_generado
+
+def mover_letra_inicial(abecedario_generado, letraInicial):
+    letraInicial = letraInicial.upper()
+    
+    if letraInicial in abecedario_generado:
+        index = abecedario_generado.index(letraInicial)
+        abecedario_generado = abecedario_generado[index:] + abecedario_generado[:index]
+    
+    return abecedario_generado
+
+def aplicar_palabra_clave(abecedario_generado, palabraClave):
+    palabraClave = palabraClave.upper() 
+    
+    palabraClave = "".join(sorted(set(palabraClave), key=palabraClave.index)) #Quitar letras repetidas
+    abecedario_generado = "".join([letra for letra in abecedario_generado if letra not in palabraClave]) #Quitar letras de la palabra clave del abecedario
+    
+    return palabraClave + abecedario_generado
 
 def pagina_no_encontrada(error):
     return render_template("404.html"), 404
